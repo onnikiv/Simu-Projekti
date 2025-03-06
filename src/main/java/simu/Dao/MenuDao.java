d34d3453ff443f56f513f525dabc632e0339a908
TonyKarlin <tony.karlin@metropolia.fi>
2025-03-07 01:49:33 +0200

    randomMeals in three different methods (for now?? cba maybe idk)

--- src/main/java/simu/Dao/MenuDao.java
+++ src/main/java/simu/Dao/MenuDao.java
@@ -26,6 +26,14 @@ public class MenuDao {
         return randomStarter();
     }
 
+    public MenuItem getRandomMainMeal() {
+        return randomMainMeal();
+    }
+
+    public MenuItem getRandomDessert() {
+        return randomDessert();
+    }
+
     private List<MenuItem> getMenuItemsByCategory(int categoryId) {
         List<MenuItem> menuItems = new ArrayList<>();
         String query = "SELECT id, name, prep_time_minutes FROM menu_items WHERE category_id = '" + categoryId + "'";
@@ -42,7 +50,33 @@ public class MenuDao {
     }
 
     private MenuItem randomStarter() {
-        String query = "SELECT id, name, prep_time_minutes FROM menu_items WHERE category_id = '1' ORDER BY RAND() LIMIT 1";
+        String query = "SELECT id, name, prep_time_minutes FROM menu_items WHERE category_id = 1 ORDER BY RAND() LIMIT 1";
+        try (Statement s = conn.createStatement();
+             ResultSet rs = s.executeQuery(query)) {
+            if (rs.next()) {
+                return new MenuItem(rs.getInt("id"), rs.getString("name"), rs.getInt("prep_time_minutes"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private MenuItem randomMainMeal() {
+        String query = "SELECT id, name, prep_time_minutes FROM menu_items WHERE category_id = 2 ORDER BY RAND() LIMIT 1";
+        try (Statement s = conn.createStatement();
+             ResultSet rs = s.executeQuery(query)) {
+            if (rs.next()) {
+                return new MenuItem(rs.getInt("id"), rs.getString("name"), rs.getInt("prep_time_minutes"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private MenuItem randomDessert() {
+        String query = "SELECT id, name, prep_time_minutes FROM menu_items WHERE category_id = 3 ORDER BY RAND() LIMIT 1";
         try (Statement s = conn.createStatement();
              ResultSet rs = s.executeQuery(query)) {
             if (rs.next()) {
