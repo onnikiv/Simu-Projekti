02b905e2fc5191fcdd40a47e365a13821a6e22f7
onnik <onni.kivinen@gmail.com>
2025-03-08 20:42:19 +0200

    Add getCustomerAmountServed method to IVisualization interface

--- src/main/java/view/IVisualization.java
+++ src/main/java/view/IVisualization.java
@@ -7,7 +7,9 @@ public interface IVisualization {
 
 	public void newCustomer(int customer);
 
+	// jotta saadaan controlleriin luvut, mitkä controller sit lisää käyttöliittymään
 	public int getCustomerAmount();
+	public int getCustomerAmountServed();
 
 	public void removeCustomer(int customer);
 
