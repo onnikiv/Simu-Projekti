2dfda17e276156837888e7a55928ab9226dca961
onnik <onni.kivinen@gmail.com>
2025-03-10 18:35:38 +0200

    controlleri saa arvot

--- src/main/java/controller/IControllerForM.java
+++ src/main/java/controller/IControllerForM.java
@@ -8,5 +8,6 @@ public interface IControllerForM {
     public void visualizeCustomer(int customer);;
 
     public void visualizeRemoveCustomers(int customer);
-
+    
+    public void updateServicePointSums(int c0, int c1, int c2, int c3,int c4,int c5);
 }
