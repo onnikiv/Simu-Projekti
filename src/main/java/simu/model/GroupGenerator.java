91893632dfa9b5d0851bb663d69e5247b0138155
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-03-13 12:01:14 +0200

    kommenttei

--- src/main/java/simu/model/GroupGenerator.java
+++ src/main/java/simu/model/GroupGenerator.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * GroupGenerator class that generates a group of customers.
+ * @see Customer Class that is part of the group.
+ *
+ * @version 1.0
+ */
 public class GroupGenerator {
     private int groupId;
 
+    /**
+     * Constructs a GroupGenerator object with the specified group ID.
+     *
+     * @param maxSize Gets the randomly generated group size.
+     * @return Returns the generated group of Customers.
+     */
     public List<Customer> generateCustomerGroup(int maxSize) {
         int size = groupSize(maxSize);
         List<Customer> group = new ArrayList<>();
@@ -22,6 +34,11 @@ public class GroupGenerator {
         return group;
     }
 
+    /**
+     * Generates a random group size.
+     * @param n Parameter for maximum group size.
+     * @return Random group size.
+     */
     public int groupSize(int n) {
         return new Random().nextInt(n) + 1;
     }
